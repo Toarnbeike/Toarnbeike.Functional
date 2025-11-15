6dd439c97b82f1bf8ca0954e293a40cb18ec196d
Toarnbeike <toarnbeike@outlook.com>
2025-11-15 23:45:10 +0100

    Added linq extensions with tests

---
+++ src/Functional/Eithers/Linq/LinqExtensions.cs
@@ -0,0 +1,59 @@
+﻿using Toarnbeike.Eithers.Extensions;
+
+namespace Toarnbeike.Eithers.Linq;
+
+public static class LinqExtensions
+{
+    extension<TLeft, TRight>(Either<TLeft, TRight> either)
+    {
+        /// <summary>
+        /// Projects the right value of the specified <see cref="Either{TLeft, TRight}"/>
+        /// into a new form using the provided mapping function.
+        /// </summary>
+        /// <typeparam name="TResult">The type of the value the mapping function will return.</typeparam>
+        /// <param name="map">A function to apply to the right value of the <see cref="Either{TLeft, TRight}"/>.</param>
+        /// <returns>
+        /// A new <see cref="Either{TLeft, TResult}"/> containing the result of applying
+        /// the mapping function to the right value if the original <see cref="Either{TLeft, TRight}"/>
+        /// was in a right state. If the original <see cref="Either{TLeft, TRight}"/> was in a left state,
+        /// the result will contain the same left value as the original.
+        /// </returns>
+        public Either<TLeft, TResult> Select<TResult>(Func<TRight, TResult> map)
+        {
+            return either.Map(map);
+        }
+
+        /// <summary>
+        /// Projects the right value of the specified <see cref="Either{TLeft, TRight}"/> into a new form
+        /// by first binding it into a new <see cref="Either{TLeft, TMiddle}"/> using the provided binding function,
+        /// and then combining the original right value and the bound value into a final result using the projection function.
+        /// </summary>
+        /// <typeparam name="TMiddle">The type of the intermediate value returned by the binding function.</typeparam>
+        /// <typeparam name="TResult">The type of the final result produced by the projection function.</typeparam>
+        /// <param name="bind">
+        /// A function that takes the right value of the <see cref="Either{TLeft, TRight}"/> and transforms it
+        /// into a new <see cref="Either{TLeft, TMiddle}"/>.
+        /// </param>
+        /// <param name="project">
+        /// A function that takes the original right value and the intermediate value produced by the binding function,
+        /// and transforms them into the final result.
+        /// </param>
+        /// <returns>
+        /// A new <see cref="Either{TLeft, TResult}"/> containing the final result if all transformations succeed,
+        /// or containing the left value if any intermediate step results in a left state.
+        /// </returns>
+        public Either<TLeft, TResult> SelectMany<TMiddle, TResult>(
+            Func<TRight, Either<TLeft, TMiddle>> bind,
+            Func<TRight, TMiddle, TResult> project)
+        {
+            if (either.IsLeft(out var left, out var right))
+                return (Either<TLeft, TResult>)left;
+
+            var middle = bind(right);
+
+            return middle.IsLeft(out left, out var right2) 
+                ? (Either<TLeft, TResult>)left
+                : project(right, right2);
+        }
+    }
+}
---
+++ tests/Functional.Tests/Eithers/Linq/LinqExtensionsTests.cs
@@ -0,0 +1,111 @@
+﻿using Toarnbeike.Eithers.TestExtensions;
+
+namespace Toarnbeike.Eithers.Linq;
+
+public class EitherLinqTests
+{
+    private readonly Either<string, int> _left  = Either<string, int>.Left("message");
+    private readonly Either<string, int> _right = Either<string, int>.Right(3);
+
+    [Fact]
+    public void Linq_Select_Should_Map_Right()
+    {
+        var result =
+            from x in _right
+            select x * 2;
+
+        result.ShouldBeRightWithValue(6);
+    }
+
+    [Fact]
+    public void Linq_Select_Should_Preserve_Left()
+    {
+        var result =
+            from x in _left
+            select x * 2;
+
+        result.ShouldBeLeftWithValue("message");
+    }
+
+    [Fact]
+    public void Linq_SelectMany_Should_Compose_Two_Rights()
+    {
+        var other = Either<string, int>.Right(5);
+
+        var result =
+            from x in _right
+            from y in other
+            select x + y;
+
+        result.ShouldBeRightWithValue(8);
+    }
+
+    [Fact]
+    public void Linq_SelectMany_Should_Stop_On_First_Left()
+    {
+        var other = Either<string, int>.Right(5);
+
+        var result =
+            from x in _left
+            from y in other
+            select x + y;
+
+        result.ShouldBeLeftWithValue("message");
+    }
+
+    [Fact]
+    public void Linq_SelectMany_Should_Propagate_Second_Left()
+    {
+        var other = Either<string, int>.Left("second");
+
+        var result =
+            from x in _right
+            from y in other
+            select x + y;
+
+        result.ShouldBeLeftWithValue("second");
+    }
+
+    [Fact]
+    public void Linq_Nested_Computation_With_Transformations()
+    {
+        var a = Either<string, int>.Right(2);
+        var b = Either<string, int>.Right(3);
+        var c = Either<string, int>.Right(4);
+
+        var result =
+            from x in a
+            from y in b
+            from z in c
+            select x * y * z;
+
+        result.ShouldBeRightWithValue(24);
+    }
+
+    [Fact]
+    public void Linq_Nested_Computation_Stops_On_First_Left()
+    {
+        var a = Either<string, int>.Right(2);
+        var b = Either<string, int>.Left("b failed");
+        var c = Either<string, int>.Right(4);
+
+        var result =
+            from x in a
+            from y in b
+            from z in c
+            select x * y * z;
+
+        result.ShouldBeLeftWithValue("b failed");
+    }
+
+    [Fact]
+    public void Linq_SelectMany_Should_Use_Map_Like_Projection()
+    {
+        var result =
+            from x in _right
+            from y in Either<string, int>.Right(x * 10)
+            select y + 1;
+
+        result.ShouldBeRightWithValue(31);
+    }
+}
