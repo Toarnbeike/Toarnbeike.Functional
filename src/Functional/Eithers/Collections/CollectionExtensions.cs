170e94028f4c5ee7eedf1a0bd6d824ce45aa482e
Toarnbeike <toarnbeike@outlook.com>
2025-11-15 20:07:21 +0100

    Sync collection extensions with tests

---
+++ src/Functional/Eithers/Collections/CollectionExtensions.cs
@@ -0,0 +1,143 @@
+﻿namespace Toarnbeike.Eithers.Collections;
+
+public static class CollectionExtensions
+{
+    extension<TValue>(IEnumerable<TValue> collection)
+    {
+        /// <summary>
+        /// Applies a function to each element in the collection, producing an <see cref="Either{TLeft, TRight}"/> for each element.
+        /// If any operation results in a left value, the process short-circuits and returns that left value.
+        /// Otherwise, returns a right value containing a collection of all successfully transformed results.
+        /// </summary>
+        /// <typeparam name="TLeft">The type of the left value, typically representing an error.</typeparam>
+        /// <typeparam name="TRight">The type of the right value, typically representing a successful result.</typeparam>
+        /// <param name="bind">The function to apply to each element in the collection, producing an <see cref="Either{TLeft, TRight}"/>.</param>
+        /// <returns>An <see cref="Either{TLeft, TRight}"/> where the left represents an error occurred during processing and the right contains a collection of all successful results.</returns>
+        public Either<TLeft, IEnumerable<TRight>> Traverse<TLeft, TRight>(Func<TValue, Either<TLeft, TRight>> bind)
+        {
+            ArgumentNullException.ThrowIfNull(collection);
+
+            List<TRight> rights = [];
+            foreach (var value in collection)
+            {
+                var either = bind(value);
+                if (either.IsLeft(out var left, out var right)) return Either<TLeft, IEnumerable<TRight>>.Left(left);
+                rights.Add(right);
+            }
+            return Either<TLeft, IEnumerable<TRight>>.Right(rights);
+        }
+    }
+    
+    extension<TLeft, TRight>(IEnumerable<Either<TLeft, TRight>> collection)
+    {
+        // ReSharper disable InvalidXmlDocComment -- Justification: XML documentation of nested generics are disliked.
+        /// <summary>
+        /// Converts a collection of <see cref="Either{TLeft, TRight}"/> values into a single
+        /// <see cref="Either{TLeft, IEnumerable{TRight}}"/>. If any element in the collection
+        /// represents a left value, the result is a left containing the first left value encountered.
+        /// Otherwise, the result is a right containing a collection of all right values.
+        /// </summary>
+        /// <typeparam name="TLeft">The type of the left value in the <see cref="Either{TLeft, TRight}"/>.</typeparam>
+        /// <typeparam name="TRight">The type of the right value in the <see cref="Either{TLeft, TRight}"/>.</typeparam>
+        /// <returns>
+        /// A <see cref="Either{TLeft, IEnumerable{TRight}}"/> containing either the first left value
+        /// encountered in the input collection or a collection of all right values if none of the
+        /// inputs are left values.
+        /// </returns>
+        // ReSharper restore InvalidXmlDocComment
+        public Either<TLeft, IEnumerable<TRight>> Sequence() => collection.Traverse(static x => x);
+
+        /// <summary>
+        /// Executes the specified action on the right values of each <see cref="Either{TLeft, TRight}"/>
+        /// in the collection. If an element in the collection is a right, the action is invoked with
+        /// the right value as its argument.
+        /// </summary>
+        /// <typeparam name="TLeft">The type of the left value in the <see cref="Either{TLeft, TRight}"/>.</typeparam>
+        /// <typeparam name="TRight">The type of the right value in the <see cref="Either{TLeft, TRight}"/>.</typeparam>
+        /// <param name="tap">The action to be invoked on the right values of the collection elements.</param>
+        public void TapAll(Action<TRight> tap)
+        {
+            ArgumentNullException.ThrowIfNull(collection);
+            
+            foreach (var either in collection)
+            {
+                if (either.IsRight(out var right)) tap(right);
+            }
+        }
+
+        /// <summary>
+        /// Determines whether any element in the collection represents a left value.
+        /// </summary>
+        /// <returns> <c>true</c> if any element in the collection is a left value; otherwise, <c>false</c>. </returns>
+        public bool AnyLeft() => collection.Any(either => either.IsLeft(out _));
+
+        /// <summary>
+        /// Determines whether any element in the collection represents a right value.
+        /// </summary>
+        /// <returns> <c>true</c> if any element in the collection is a right value; otherwise, <c>false</c>. </returns>
+        public bool AnyRight() => collection.Any(either => either.IsRight(out _));
+
+        /// <summary>
+        /// Counts the number of elements in the collection that represent left values.
+        /// </summary>
+        public int CountLeft() => collection.Count(either => either.IsLeft(out _));
+        
+        /// <summary>
+        /// Counts the number of elements in the collection that represent right values.
+        /// </summary>
+        public int CountRight() => collection.Count(either => either.IsRight(out _));
+
+        /// <summary>
+        /// Extracts all left values from a collection of <see cref="Either{TLeft, TRight}"/> values.
+        /// </summary>
+        /// <typeparam name="TLeft">The type of the left value in the <see cref="Either{TLeft, TRight}"/>.</typeparam>
+        /// <typeparam name="TRight">The type of the right value in the <see cref="Either{TLeft, TRight}"/>.</typeparam>
+        /// <returns> An <see cref="IEnumerable{T}"/> that contains all left values in the collection. </returns>
+        public IEnumerable<TLeft> Lefts()
+        {
+            foreach (var either in collection)
+            {
+                if (either.IsLeft(out var left)) yield return left;
+            }
+        }
+
+        /// <summary>
+        /// Retrieves all right values from a collection of <see cref="Either{TLeft, TRight}"/> values.
+        /// Only elements that represent a right value are included in the output sequence.
+        /// </summary>
+        /// <typeparam name="TLeft">The type of the left value in the <see cref="Either{TLeft, TRight}"/>.</typeparam>
+        /// <typeparam name="TRight">The type of the right value in the <see cref="Either{TLeft, TRight}"/>.</typeparam>
+        /// <returns> An <see cref="IEnumerable{TRight}"/> containing all right values in the collection. </returns>
+        public IEnumerable<TRight> Rights()
+        {
+            foreach (var either in collection)
+            {
+                if (either.IsRight(out var right)) yield return right;
+            }
+        }
+
+        /// <summary>
+        /// Splits a collection of <see cref="Either{TLeft, TRight}"/> instances into two separate collections,
+        /// one containing all left values and the other containing all right values.
+        /// </summary>
+        /// <typeparam name="TLeft">The type of the left value in the <see cref="Either{TLeft, TRight}"/>.</typeparam>
+        /// <typeparam name="TRight">The type of the right value in the <see cref="Either{TLeft, TRight}"/>.</typeparam>
+        /// <returns>
+        /// A tuple where the first element is an <see cref="IEnumerable{TLeft}"/> containing all left values and
+        /// the second element is an <see cref="IEnumerable{TRight}"/> containing all right values.
+        /// </returns>
+        public (IEnumerable<TLeft> lefts, IEnumerable<TRight> rights) Partition()
+        {
+            var lefts = new List<TLeft>();
+            var rights = new List<TRight>();
+            
+            foreach (var either in collection)
+            {
+                if (either.IsLeft(out var left, out var right)) lefts.Add(left);
+                else rights.Add(right);
+            }
+            
+            return (lefts, rights);
+        }
+    }
+}
---
+++ tests/Functional.Tests/Eithers/Collection/CollectionExtensionsTests.cs
@@ -0,0 +1,155 @@
+﻿using Toarnbeike.Eithers.Collections;
+using Toarnbeike.Eithers.TestExtensions;
+
+namespace Toarnbeike.Eithers.Collection;
+
+public class CollectionExtensionsTests
+{
+    private readonly IEnumerable<Either<string, int>> _allRights;
+    private readonly IEnumerable<Either<string, int>> _mixed;
+    private readonly IEnumerable<Either<string, int>> _allLefts;
+    private readonly IEnumerable<int> _ints = [1,2,3];
+
+    public CollectionExtensionsTests()
+    {
+        var right1 = Either<string, int>.Right(1);
+        var right2 = Either<string, int>.Right(2);
+        var right3 = Either<string, int>.Right(3);
+        var leftA =  Either<string, int>.Left("A");
+        var leftB =  Either<string, int>.Left("B");
+        
+        _allRights = [right1, right2, right3];
+        _mixed = [right1, right2, leftA, right3, leftB];
+        _allLefts = [leftA, leftB];
+    }
+
+    [Fact]
+    public void Traverse_Should_Throw_WhenInputIsNull()
+    {
+        IEnumerable<int>? nullCollection = null;
+        Should.Throw<ArgumentNullException>(() => nullCollection!.Traverse(Either<string, int>.Right));
+    }
+
+    [Fact]
+    public void Traverse_Should_CreateRightSequence_WhenAllRight()
+    {
+        var traversed = _ints.Traverse((Func<int, Either<string, int>>)Bind);
+        
+        traversed.ShouldBeOfType<Either<string, IEnumerable<int>>>();
+        var actual = traversed.ShouldBeRight();
+        actual.ShouldBe(_ints.Select(value => value * 2));
+        return;
+
+        Either<string, int> Bind(int value) => 
+            Either<string, int>.Right(value * 2);
+    }
+
+    [Fact]
+    public void Traverse_Should_CreateLeftValue_WhenItemIsLeft()
+    {
+        var traversed = _ints.Traverse((Func<int, Either<string, int>>)Bind);
+        
+        traversed.ShouldBeOfType<Either<string, IEnumerable<int>>>();
+        var actual = traversed.ShouldBeLeft();
+        actual.ShouldBe("too high");
+        return;
+
+        Either<string, int> Bind(int value) => 
+            value < 3 ? Either<string, int>.Right(value * 2) : Either<string, int>.Left("too high");
+    }
+
+    [Fact]
+    public void Sequence_Should_CreateRightSequence_WhenAllRight()
+    {
+        var actual = _allRights.Sequence().ShouldBeRight();
+        actual.ShouldBe([1,2,3]);
+    }
+    
+    [Fact]
+    public void Sequence_Should_CreateLeftValue_WhenItemIsLeft()
+    {
+        _mixed.Sequence().ShouldBeLeftWithValue("A");
+    }
+
+    [Fact]
+    public void TapAll_Should_ApplyToAllRightValues()
+    {
+        var sum = 0;
+        _allRights.TapAll(Increase);
+        sum.ShouldBe(6);
+        return;
+
+        void Increase(int value) => sum += value;
+    }
+
+    [Fact]
+    public void TapAll_Should_WorkAlsoOnMixedCollection()
+    {
+        var sum = 0;
+        _mixed.TapAll(Increase);
+        sum.ShouldBe(6);
+        return;
+
+        void Increase(int value) => sum += value;
+    }
+    
+    [Fact]
+    public void AnyLeft_Should_ReturnTrue_WhenLeft()
+    {
+        _mixed.AnyLeft().ShouldBeTrue();
+    }
+    
+    [Fact]
+    public void AnyLeft_Should_ReturnFalse_WhenRight()
+    {
+        _allRights.AnyLeft().ShouldBeFalse();
+    }
+    
+    [Fact]
+    public void AnyRight_Should_ReturnTrue_WhenRight()
+    {
+        _mixed.AnyRight().ShouldBeTrue();
+    }
+    
+    [Fact]
+    public void AnyRight_Should_ReturnFalse_WhenLeft()
+    {
+        _allLefts.AnyRight().ShouldBeFalse();
+    }
+    
+    [Fact]
+    public void CountLeft_Should_ReturnCountOfLefts()
+    {
+        _mixed.CountLeft().ShouldBe(2);
+        _allRights.CountLeft().ShouldBe(0);
+    }
+    
+    [Fact]
+    public void CountRight_Should_ReturnCountOfRights()
+    {
+        _mixed.CountRight().ShouldBe(3);
+        _allLefts.CountRight().ShouldBe(0);
+    }
+
+    [Fact]
+    public void Lefts_Should_ReturnLefts()
+    {
+        var result = _mixed.Lefts();
+        result.ShouldBe(["A", "B"]);
+    }
+    
+    [Fact]
+    public void Rights_Should_ReturnRights()
+    {
+        var result = _mixed.Rights();
+        result.ShouldBe([1,2,3]);
+    }
+
+    [Fact]
+    public void Partition_Should_ReturnPartition()
+    {
+        var (lefts, rights) = _mixed.Partition();
+        lefts.ShouldBe(["A", "B"]);
+        rights.ShouldBe([1,2,3]);
+    }
+}
